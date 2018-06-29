d770366f6014a1a7798dc656bc9d11a236ac41ea
lololobo93 <lololobo93@ciencias.unam.mx>
2018-06-29 18:16:25 -0500

    Capítulo 24 y 25

---
+++ Soluciones/cap24_1.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+class TicTacToe{
+
+private:
+  char table[3][3];
+  char winner(char array[3][3]);
+  void imprimir(char array[3][3]);
+
+public:
+  void iniciar();
+  void moves();
+
+};
+
+void TicTacToe::iniciar(){
+  for (int i = 0; i < 3; i++) {
+    /* code */
+    for (int j = 0; j < 3; j++) {
+      /* code */
+      table[i][j] = '/';
+      std::cout << table[i][j];
+      if (j != 2) {
+        /* code */
+        std::cout << '|';
+      } else {
+        std::cout << '\n';
+      }
+    }
+    if (i != 2) {
+      /* code */
+      std::cout << "-----" << '\n';
+    }
+  }
+}
+
+void TicTacToe::imprimir(char array[3][3]) {
+  /* code */
+  for (int i = 0; i < 3; i++) {
+    /* code */
+    for (int j = 0; j < 3; j++) {
+      /* code */
+      std::cout << array[i][j];
+      if (j != 2) {
+        /* code */
+        std::cout << '|';
+      } else {
+        std::cout << '\n';
+      }
+    }
+    if (i != 2) {
+      /* code */
+      std::cout << "-----" << '\n';
+    }
+  }
+}
+
+char TicTacToe::winner(char array[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    /* code */
+    if (array[i][0] == array[i][1] && array[i][1] == array[i][2]) {
+      /* code */
+      return array[i][0];
+      break;
+    }
+    if (array[0][i] == array[1][i] && array[1][i] == array[2][i]) {
+      /* code */
+      return array[0][i];
+      break;
+    }
+  }
+  if (array[0][0] == array[1][1] && array[1][1] == array[2][2]) {
+    /* code */
+    return array[0][0];
+  } else if (array[0][2] == array[1][1] && array[1][1] == array[2][0]) {
+    /* code */
+    return array[2][0];
+  } else {
+    return '/';
+  }
+}
+
+void TicTacToe::moves(){
+  int pos1, pos2, count;
+  bool comp;
+  char res;
+  for (int i = 0; i < 5; i++) {
+    /* code */
+    comp = false;
+    do {
+      /* code */
+      std::cout << "Turno jugador uno. Dime posición. " << '\n';
+      std::cin >> pos1;
+      std::cin >> pos2;
+      if (table[pos1][pos2] == '/') {
+        table[pos1][pos2] = 'x';
+        comp = true;
+      } else {
+        std::cout << "Posición ocupada" << '\n';
+      }
+    } while(comp == false);
+    std::cout << "Tablero: " << '\n';
+    imprimir(table);
+
+    res = winner(table);
+
+    if (res != '/') {
+      /* code */
+      break;
+    }
+    if (i != 4) {
+      /* code */
+      comp = false;
+      do {
+        /* code */
+        std::cout << "Turno jugador dos. Dime posición. " << '\n';
+        std::cin >> pos1;
+        std::cin >> pos2;
+        if (table[pos1][pos2] == '/') {
+          table[pos1][pos2] = 'o';
+          comp = true;
+        } else {
+          std::cout << "Posición ocupada/inválida" << '\n';
+        }
+      } while(comp == false);
+      std::cout << "Tablero: " << '\n';
+      imprimir(table);
+      res = winner(table);
+
+      if (res != '/') {
+        /* code */
+        break;
+      }
+
+    }
+  }
+  if (res != '/') {
+    std::cout << "Ganador es: " << res << '\n';
+  } else {
+    std::cout << "Par de perdedores." << '\n';
+  }
+}
+
+int main() {
+  /* code */
+  bool game = true;
+  std::cout << "Tablero: " << '\n';
+  TicTacToe NuevoJuego;
+  NuevoJuego.iniciar();
+  NuevoJuego.moves();
+  return 0;
+}
